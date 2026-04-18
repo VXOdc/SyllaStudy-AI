e4a86b9f0eefb91a99b498e7f6a11c2b3c9efcaa
VXOdc <124010810+VXOdc@users.noreply.github.com>
2026-04-17 17:41:34 -0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,7 +1,10 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-
 const supabaseUrl = window.SUPABASE_URL;
 const supabaseAnonKey = window.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase env variables missing");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
