edee7cf97831b074928297dfed714fcf8d6bfbc0
VXOdc <124010810+VXOdc@users.noreply.github.com>
2026-04-17 18:42:52 -0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -7,7 +7,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase URL or ANON KEY");
 }
 
-export const supabase = createClient(https://qyqvuukigsbjuhjygrxp.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5cXZ1dWtpZ3NianVoanlncnhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNjEzMjAsImV4cCI6MjA5MTkzNzMyMH0.5ivvnQC5f-6Yp62ptBt7jZpK_4c-WIwfPnNkbfh9_4s);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-
+// allow console testing
 window.supabase = supabase;
